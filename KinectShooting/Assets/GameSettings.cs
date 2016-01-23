687a2d8f674e4ba1650079e502c7df583f0d3b2a
oliverfei <ofei006@uottawa.ca>
2016-01-23 11:42:13 -0500

    Added some scripts

---
+++ KinectShooting/Assets/GameSettings.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings{
+    //Game modes
+    public static readonly int TIMED = 1;
+    public static readonly int COUNT = 2;
+}
---
+++ KinectShooting/Assets/KinectManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class KinectManager: MonoBehaviour {
+
+    public static bool kinectEnabled = false;
+	private KinectSensor _Sensor;
+	private BodyFrameReader _Reader;
+	private Body[] _Data = null;
+
+	// Use this for initialization
+	void Start () {
+		_Sensor = KinectSensor.GetDefault();
+
+		if (_Sensor != null)
+		{
+			_Reader = _Sensor.BodyFrameSource.OpenReader();
+
+			if (!_Sensor.IsOpen)
+			{
+				_Sensor.Open();
+			}
+            kinectEnabled = true;
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_Reader != null)
+		{
+			var frame = _Reader.AcquireLatestFrame();
+
+			if (frame != null)
+			{
+				if (_Data == null)
+				{
+					_Data = new Body[_Sensor.BodyFrameSource.BodyCount];
+				}
+
+				frame.GetAndRefreshBodyData(_Data);
+
+				frame.Dispose();
+				frame = null;
+			}
+		}
+	}
+
+	void OnApplicationQuit()
+	{
+		if (_Reader != null)
+		{
+			_Reader.Dispose();
+			_Reader = null;
+		}
+
+		if (_Sensor != null)
+		{
+			if (_Sensor.IsOpen)
+			{
+				_Sensor.Close();
+			}
+			_Sensor = null;
+		}
+	}
+
+    public Body[] getData()
+    {
+        return _Data;
+    }
+
+    public int getNumBodies()
+    {
+        return _Sensor.BodyFrameSource.BodyCount;
+    }
+}
