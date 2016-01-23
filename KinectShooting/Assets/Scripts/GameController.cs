1065aebd6008e9bbfeb2aa4a6fb68e526dc666ec
oliverfei <ofei006@uottawa.ca>
2016-01-23 15:44:59 -0500

    Update scene

--- KinectShooting/Assets/Scripts/GameController.cs
+++ KinectShooting/Assets/Scripts/GameController.cs
@@ -20,6 +20,7 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Cursor.visible = false;
+        nextRespawn = Random.Range(respawnSpeed - 1, respawnSpeed + 1);
     }
 	
 	// Update is called once per frame
